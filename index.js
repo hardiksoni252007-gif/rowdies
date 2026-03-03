f0973fd8cfeb20174ab1089dd6500e2531f2e370
Hardik <hardiksoni252007@gmail.com>
2026-03-03 13:11:40 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,62 @@
+function Login(){
+    let user = document.getElementById("userName").value;
+    let pass = document.getElementById("passWord").value;
+    let face = document.getElementById("face");
+    let lock = document.getElementById("lock");
+    let msg = document.getElementById("msg");
+
+    let username = "Logic";
+    let password = "@000";
+
+    if(user == username && pass == password){
+        window.location.href = "/college_web/main.html";
+        face.style.color="darkgreen";
+        lock.style.color="darkgreen";
+        msg.innerHTML="Login";
+        msg.style.color="darkgreen";
+    }
+    
+    else if(user == username && pass != password){
+        face.style.color="darkgreen";
+        lock.style.color="maroon";
+    }
+    
+    else if(user != username && pass == password){
+        lock.style.color="darkgreen";
+        face.style.color="maroon";
+    }
+    
+    else if(user == "" && pass == ""){
+        face.style.color="maroon";
+        lock.style.color="maroon";
+        msg.innerHTML="Enter Details";
+        msg.style.color="maroon";
+        
+    }
+    
+    else if(user != username && pass != password){
+        lock.style.color="maroon";
+        face.style.color="maroon";
+        msg.innerHTML="Wrong Details";
+        msg.style.color="maroon";
+    }
+}
+
+let contain = document.getElementById("contain")
+contain.style.background="linear-gradient(345deg,rgba(255, 0, 55, 1) 0%,rgba(255, 75, 0, 0.9) 35%,rgba(255, 135, 75, 0.9) 70%,rgba(105, 0, 75, 1) 100%)";
+
+// const button = document.getElementById("login");
+
+//     document.addEventListener("mousemove", (e) => {
+//       const rect = button.getBoundingClientRect();
+//       const distanceX = e.clientX - (rect.left + rect.width / 2);
+//       const distanceY = e.clientY - (rect.top + rect.height / 2);
+//       const distance = Math.sqrt(distanceX**2 + distanceY**2);
+
+//       // If cursor is too close, move the button
+//       if (distance < 100) {
+//         const offsetX = (Math.random() - 0.5) * 300;
+//         const offsetY = (Math.random() - 0.5) * 300;
+//         button.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+//       }
+//     });
---
+++ main.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+    // Show "Login successful" initially
+    document.getElementById("detail").innerText = "Login successful";
+    document.getElementById("detail").style.color = "green";
+      
+    // After 3 seconds
+    setTimeout(function() {
+        document.getElementById("detail").innerText = "Welcome to our website dashboard";
+        document.getElementById("detail").style.color = "black"
+    }, 1000); // 1000 milliseconds = 1 seconds
+
+};
+// let dash = document.getElementById('dash');
+// let link = document.getElementById('link');
+// let dev = document.getElementById('dev');
+// let port = document.getElementById('port');
+
+// if(window.innerWidth < 921){
+//     dev.innerText = "Dashboard"
+// }
+// else{
+//     dev.addEventListener('click', ()=>{
+//         window.location = "portfolio.html"
+//     });
+// }
+
+// dev.addEventListener('click', ()=>{
+//     if (window.innerWidth < 921) {
+//         port.style.display = "flex"
+//         dash.style.display = "flex"
+//         link.style.display = "none"
+//     } 
+
+// })
+
